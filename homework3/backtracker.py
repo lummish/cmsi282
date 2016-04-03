959c226e9cc0cd5f8825db3e164a4968581028cb
lummish <lummish@gmail.com>
2016-04-03 14:30:20 -0700

    correctly parameterized backtracking engine

--- homework3/backtracker.py
+++ homework3/backtracker.py
@@ -14,6 +14,24 @@ def backtracker(toTest, maxCandVal, solutionSize, isCorrect):
 			toTest[current_branch_idx] += 1
 			toTest = toTest[:current_branch_idx + 1]
 	 
+def backtrackerNew(toTest, possibleElements, solutionSize, isCorrect):
+	if not isCorrect(toTest):
+		return
+	elif solutionSize == len(toTest):
+		return toTest
+	else:
+		toTest += [possibleElements[0]]
+		cur_poss_idx = 0
+		current_branch_idx = len(toTest) - 1
+		while True:
+			toReturn = backtrackerNew(toTest, possibleElements, solutionSize, isCorrect)
+			if toReturn:
+				return toReturn #return if toReturn is not None
+			if cur_poss_idx == len(possibleElements) - 1:
+				break
+			cur_poss_idx += 1
+			toTest[current_branch_idx] = possibleElements[cur_poss_idx] #set current branch to next element in possibleElements
+			toTest = toTest[:current_branch_idx + 1]
 
 def isQCorrect(toTest):
 	if len(toTest) > len(set(toTest)): # return false if not all rows unique
@@ -40,5 +58,6 @@ def isNEASCorrect(toTest):
 
 
 print(backtracker([], 8, 8, isQCorrect))
-print("".join(str(i) for i in backtracker([], 3, 50, isCorrectNEAS)))
+print(backtrackerNew([], [0,1,2,3,4,5,6,7], 8, isQCorrect))
+print("".join(str(i) for i in backtrackerNew([], [0,1,2], 50, isNEASCorrect)))
 print("01020120210120102012021020102101201020120210120102")
