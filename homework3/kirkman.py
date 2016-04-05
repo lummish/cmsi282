cf12e8668bd8f2adfa8c938bcc10776be47c9790
lummish <lummish@gmail.com>
2016-04-05 05:34:38 -0700

    slow but seemingly functional kirkmansolver

--- homework3/kirkman.py
+++ homework3/kirkman.py
@@ -1,91 +1,77 @@
-#kirkman.py
-'''
-Solution will take the form:
-[[[0,1,2],
-  [3,4,5],
-  [6,7,8],
-  [9,10,11],
-  [12,13,14]],
- [[next day matrix],...]
-'''
-def kirkmanBacktracker(toTest, total_days, isCorrect):
-	print(toTest)
+# kirkiter.py
+import itertools
+
+
+
+def backtracker(toTest, possibleElements, solutionSize, isCorrect):
 	if not isCorrect(toTest):
 		return
-	elif (total_days == len(toTest) and 5 == len(toTest[-1]) and 
-		  3 == len(toTest[-1][-1])):
+	elif solutionSize == len(toTest):
 		return toTest
 	else:
-		# -1 is used as a placeholder element that will simplify the process of 
-		# getting the current_branch_idx
-		if len(toTest) == 0 or (len(toTest[-1]) == 5 and len(toTest[-1][-1]) == 3): 
-			print("new day")
-			# if there are no days yet or a new day must be added
-			day_idx = len(toTest)
-			row_idx = 0
-			girl_idx = 0
-
-			toTest += [[[0]]] # begin new 2d array
-
-		else:
-			if len(toTest[-1][-1]) == 3 and len(toTest[-1]) < 5:
-				# if a new row must be added to the current day, add one
-				day_idx = len(toTest) - 1
-				row_idx = len(toTest[-1])
-				girl_idx = 0
-
-				toTest[-1] += [[0]]	
-			else:
-				# if there is space remaining in the current row, add an element
-				day_idx = len(toTest) - 1
-				row_idx = len(toTest[-1]) - 1
-				girl_idx = len(toTest[-1][-1])
-
-				toTest[-1][-1] += [0] # maybe want to make sure its not in the current day set
-
-		cur_schoolgirl = 0 
-
+		toTest += [possibleElements[0]]
+		cur_poss_idx = first_possible(toTest)
+		last_possible_idx = last_possible(toTest)
+		current_branch_idx = len(toTest) - 1
 		while True:
-			print("totest", toTest)
-			toReturn = kirkmanBacktracker(toTest, total_days, isCorrect)
-			print("new totest", toTest)
+			toReturn = backtracker(toTest, possibleElements, solutionSize, isCorrect)
 			if toReturn:
 				return toReturn #return if toReturn is not None
-			if cur_schoolgirl == 15: #hopefully dont get here
+			if cur_poss_idx > last_possible_idx:
 				break
-			# if path fails, go to neighbor path and trim remaining from array
-			cur_schoolgirl += 1
-			toTest[day_idx][row_idx][girl_idx] = cur_schoolgirl #set current branch to next element in possibleElements
-			print("totest new schoolgirl", toTest)
-			print("cur_schoolgirl", cur_schoolgirl)
-			print("indices: ", day_idx, row_idx, girl_idx)
-			toTest = toTest[:day_idx + 2][:row_idx + 1][:girl_idx + 1] # why is this broken?????
-			print("totest", toTest)
+			cur_poss_idx += 1
+			toTest[current_branch_idx] = possibleElements[cur_poss_idx] #set current branch to next element in possibleElements
+			toTest = toTest[:current_branch_idx + 1]
 
+girl_names = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O']
+possible = list(itertools.combinations(girl_names, 3))
 
-def Correct(solution):
-	if len(solution) == 0:
+def first_possible(toTest):
+	if len(toTest) < 5:
+		return possible.index(('A', 'B', 'C'))
+	#for central first elements
+	if len(toTest) % 5 == 1:
+		next_first_center = chr(ord(toTest[-6][1]) + 1)
+		final_char = chr(ord(next_first_center) + 1)
+		return possible.index(('A', next_first_center, final_char))
+	elif len(toTest) % 5 == 2:
+		return possible.index(('B', 'C', 'D'))
+	elif len(toTest) % 5 == 3:
+		return possible.index(('C', 'D', 'E'))
+	else:
+		char_above_ord = ord(toTest[-2][0])
+		return possible.index((chr(char_above_ord + 1), chr(char_above_ord + 2),
+			chr(char_above_ord + 3)))
+
+def last_possible(toTest):
+	num_days = (len(toTest) / 5) + 1
+	if len(toTest) <= 5:
+		return 455
+	#first row central elements
+	if len(toTest) % 5 == 1:
+		middle = chr(ord('H') + num_days) # may need to reduce by 1
+		return possible.index(('A', middle, 'O'))
+	elif len(toTest) % 5 == 2:
+		return possible.index(('B', 'L', 'O'))		
+	elif len(toTest) % 5 == 3:
+		return possible.index(('C', 'L', 'O'))
+	else:
+		return possible.index(('K','L','O'))
+
+def isCorrect(toTest):
+	if len(toTest) <= 1:
 		return True
-	elif len(solution[-1][-1]) < 1:
-		return True
-	newestRow = solution[-1][-1]
-	if len(set(newestRow)) < len(newestRow):
-		print("duplicate in same row")
-		return False
-	newestElement = solution[-1][-1][-1]
-	print("solution", solution[-1][:-1])
-	for row in solution[-1][:-1]: # make sure that number has not appeared in current day		
-		if newestElement in row:
+	# check if element appears twice in day
+	day_num = (len(toTest) - 1) / 5
+	new_row_lst = list(toTest[-1])
+	for i in range(5 * day_num, len(toTest) - 1):
+		for name in new_row_lst:
+			if name in toTest[i]:
+				return False
+
+	# check if two elements have already appeared together
+	for i in range(len(toTest) - 1): # don't need to check newest one
+		if len(set(list(toTest[i]) + new_row_lst)) < 5:
 			return False
-	if len(newestRow) > 1: # tests for correctness ONLY when two elements are in the same row
-		for day in solution[:-1]: #wont need to test current day
-			for row in day:
-				# If there are two or more non unique elements relative to another row, 
-				# must not be a viable solution (i.e. if two elements have already appeared
-				# in the same row, discard current solution)
-				if len(set(row + newestRow)) < len(row) + len(newestRow) - 1:
-					print("duplicate")
-					return False
-	return True
 
-print(kirkmanBacktracker([], 7, Correct))
+	return True
