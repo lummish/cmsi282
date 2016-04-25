2b2dc3d0efe0c5291cd7fee88639212626dbd2ec
lummish <lummish@gmail.com>
2016-04-25 13:20:18 -0700

    fixed minimization issues, probably

--- homework4/Changer.java
+++ homework4/Changer.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class Changer {
 	private int[] denoms;
 	public Changer(int[] denominations) {
@@ -6,6 +8,7 @@ public class Changer {
 		for (int i = 0; i < denominations.length; i++) {
 			denoms[i + 1] = denominations[i];
 		}
+		Arrays.sort(denoms); //should make it simpler to minimize coin amounts in max_coins
 	}
 
 	public boolean can_make_change_for(int amount) {
@@ -54,10 +57,60 @@ public class Changer {
 	}
 
 	public boolean can_make_change_with_limited_coins(int amount, int max_coins) {
-		return false;
+		if (amount == 0) {
+			return true;
+		}
+		int[][] grid = new int[denoms.length][amount + 1];
+		for (int i = 0; i < denoms.length; i++) {
+			for (int j = 0; j <= amount; j++) {
+				grid[i][j] = -1; //initialize to negative values for later checking
+			}
+		}
+		grid[0][0] = 0;
+		for (int i = 1; i < denoms.length; i++) {
+			for (int j = 0; j <= amount; j++) {
+				if (j - denoms[i] < 0) {
+					grid[i][j] = grid[i - 1][j];
+				}
+				else {
+					int add_coin = -1;
+					int no_new_coin = -1;
+
+					if (grid[i - 1][j - denoms[i]] >= 0) {
+						if (grid[i][j - denoms[i]] >= 0) {
+							add_coin = grid[i - 1][j - denoms[i]] < grid[i][j - denoms[i]] ? grid[i - 1][j - denoms[i]] : grid[i][j - denoms[i]]; //minimize number of coins (possible that current row number is min)
+							add_coin++;
+						}
+						else {
+							add_coin = grid[i - 1][j - denoms[i]] + 1;
+						}
+						
+					} 
+					if (j % denoms[i] == 0) {
+						//if current coin can be added multiple times (greedy, will minimize coin amt)
+						add_coin = j / denoms[i]; 
+					} 
+					else {
+						no_new_coin = grid[i - 1][j];
+					}
+
+					if (add_coin >= 0 && no_new_coin >= 0) {
+						//set grid position to minimum coin value for that sum
+						grid[i][j] = (add_coin < no_new_coin ? add_coin : no_new_coin); 
+					}
+
+					else {
+						//if one option is impossible, return the possible one
+						grid[i][j] = (add_coin > no_new_coin ? add_coin : no_new_coin); 
+					}
+				}
+			}
+		}
+		//print_int_grid(grid, amount, denoms); //for testing purposes 
+		return grid[denoms.length - 1][amount] <= max_coins;
 	}
 
-	public static void print_grid(boolean[][] grid, int amount, int[] denoms) {
+	public static void print_boolean_grid(boolean[][] grid, int amount, int[] denoms) {
 		for (int i = 0; i <= amount; i++) {
 			if (i == 0) {
 				System.out.print("   ");
@@ -78,6 +131,23 @@ public class Changer {
 			System.out.println();
 		}
 	}
+
+	public static void print_int_grid(int[][] grid, int amount, int[] denoms) {
+		for (int i = 0; i <= amount; i++) {
+			if (i == 0) {
+				System.out.print("   ");
+			}
+			System.out.format("%02d ", i);
+		}
+		System.out.println();
+		for (int i = 0; i < denoms.length; i++) {
+			System.out.print(i + ": ");
+			for (int j = 0; j <= amount; j++) {
+				System.out.format("%02d ", grid[i][j]);
+			}
+			System.out.println();
+		}
+	}
 	public static void main(String[] args) {
 		int[] denoms = {2,4,9};
 
@@ -86,6 +156,14 @@ public class Changer {
 		System.out.println(c.can_make_change_for(0));
 		System.out.println(c.can_make_change_for(12));
 		System.out.println(c.can_make_change_for(5));
+
+		int[] denoms2 = {5,10};
+		Changer c2 = new Changer(denoms2);
+
+		c2.can_make_change_with_limited_coins(35, 6);
+		c2.can_make_change_with_limited_coins(55, 6);
+		c2.can_make_change_with_limited_coins(65, 6);
+
 
 	}
 }
